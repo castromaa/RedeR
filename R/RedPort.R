77e9e68c1bbe64c6ef21096410b5ce9fae5b5be8
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-16 20:00:01 +0000

    Creating branch for BioC 3.1 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/RedeR@102593 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/RedPort.R
@@ -0,0 +1,12 @@
+
+
+#RedPort class constructor---------------------------------------------------
+
+RedPort = function (title='default', host='127.0.0.1', port=9091)
+{ 
+  uri = sprintf ('http://%s:%s', host, port)
+  dp = new ('RedPort', title=title, uri=uri, port=port)
+  return (dp)
+}
+
+
