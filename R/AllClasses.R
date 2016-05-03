96d99081717674250fdce30f3c7ba884cba7509a
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/RedeR@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/AllClasses.R
@@ -0,0 +1,25 @@
+
+#-------------------------------------------------------------------------------
+#RedeR port class--------------------------------------------------------------
+setClass ("RedPort", 
+          representation = representation (title  = "character",
+                                           uri    = "character",
+                                           port   = "numeric",
+                                           jclass = "character"),
+          prototype = prototype (title  = "RedPort",
+                                 uri    = "http://127.0.0.1:9091",
+                                 port   = 9091,
+                                 jclass = "reder/rj/CanvasR"
+                                 )
+          )
+          
+#Test the validity of RRede class ---------------------------------------------
+setValidity ("RedPort",
+  function (object) {
+      c1 = length (object@title)    == 1
+      c2 = length (object@uri)      == 1 
+      c3 = length (object@port)     == 1 
+      c4 = length (object@jclass)   == 1      
+      if (c1 && c2 && c3 && c4) TRUE
+	  else cat("'title', 'uri', 'port', and 'jclass' must all have length 1")
+    })
